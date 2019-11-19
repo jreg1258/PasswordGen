e46a699837bb3ab6b66c57567e6819a7cb833431
Joey Register <josephregister1258@gmail.com>
2019-11-19 14:14:48 -0500

    logged values

---
+++ pwgen.js
@@ -0,0 +1,4 @@
+var pwLength = prompt("Pick a length between 8 and 128 characters: ")
+var spChar = confirm("Would you like to include special characters?")
+var numChar = confirm("Would you like to include numbers?")
+var upChar = confirm("Would you like to include uppercase characters as well?")
