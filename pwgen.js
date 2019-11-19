55675586406b628bd55a4ce0cfd1863ed9b3d81f
Joey Register <josephregister1258@gmail.com>
2019-11-19 15:27:01 -0500

    moved functions to pwgen.js

--- pwgen.js
+++ pwgen.js
@@ -1,4 +1,69 @@
-var pwLength = prompt("Pick a length between 8 and 128 characters: ")
-var spChar = confirm("Would you like to include special characters?")
-var numChar = confirm("Would you like to include numbers?")
-var upChar = confirm("Would you like to include uppercase characters as well?")
+function allTrue() {
+    var specials = ["0123456789!@#$%^&*?/\<>`~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+}
+
+function spUpCharTrue() {
+    var specials = ["!@#$%^&*?/\<>`~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+}
+
+function numUpCharTrue() {
+    var specials = ["0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+} 
+
+function spNumCharTrue() {
+    var specials = ["0123456789!@#$%^&*?/\<>`~abcdefghijklmnopqrstuvwxyz"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+}
+
+function spTrue() {
+    var specials = ["!@#$%^&*?/\<>`~abcdefghijklmnopqrstuvwxyz"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+}
+
+function numTrue() {
+    var specials = ["0123456789abcdefghijklmnopqrstuvwxyz"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+}
+
+function upTrue() {
+    var specials = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]
+    var stringLength = pwLength
+    var randString = " "
+    for (var i=0;i < stringLength; i++) {
+        var randNum = Math.floor(Math.random() * stringLength)
+        randString += specials.toString().substring(randNum,randNum+1)
+        } console.log(randString)
+}
