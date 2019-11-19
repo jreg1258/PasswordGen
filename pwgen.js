f61d8b3aa84ef6ac79c1964c8a55a70d6e3a00f7
Joey Register <josephregister1258@gmail.com>
2019-11-19 16:21:13 -0500

    added document.write

--- pwgen.js
+++ pwgen.js
@@ -68,6 +68,21 @@ function upTrue() {
         } console.log(randString)
 }
 
+function copyPass() {
+    /* Get the text field */
+    var copyText = document.getElementById("pwDone");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
+
 alert("Password Generator!")
 alert("You must choose between 8-128 chars, and at least one special variant for your password!")
 
